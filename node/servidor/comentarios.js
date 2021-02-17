8cac4cce948e81173486cb88ba882b636dc402e4
DiegoCarmo <deecarmo@gmail.com>
2021-02-17 13:33:37 -0300

    No-Sql

---
+++ node/servidor/comentarios.js
@@ -0,0 +1,40 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017';
+
+const database = 'fullstackstore';
+
+let client
+let db
+let comentariosCollection
+
+async function conectar() {
+  client = await MongoClient.connect(url)
+  db = client.db(database);
+  comentariosCollection = db.collection('comentarios');
+}
+
+async function inserirComentario(comentario) {
+  await conectar()
+  const result = await comentariosCollection.insertOne(comentario);
+  console.log("Inseriu o comentário");
+  client.close();
+  return result
+}
+
+async function pegarComentarios() {
+  await conectar()
+  const cursor = await comentariosCollection.find();
+  const result = []
+  await cursor.forEach(function (comentario) {
+    result.push(comentario)
+  });
+  client.close();
+  return result
+}
+
+
+
+module.exports = {
+  inserirComentario, pegarComentarios
+}
